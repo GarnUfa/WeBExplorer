c2304509d11102a7b212d5293e283d74a4aa8f6a
Garn87 <Garn87@list.ru>
2021-05-09 18:57:29 +0500

    FixedPostReqestFile

--- Explorer/Controllers/HomeController.cs
+++ Explorer/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using Explorer.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 
@@ -15,12 +19,15 @@ namespace Explorer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private ExplorerContext context;
+        private IWebHostEnvironment hostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, ExplorerContext context)
+        public HomeController(ILogger<HomeController> logger, ExplorerContext context, IWebHostEnvironment hostEnvironment)
         {
             this.context = context;
+            this.hostEnvironment = hostEnvironment;
             _logger = logger;
         }
+
         [HttpPost]
         public async Task<IActionResult> Index(string folderName)
         {
@@ -29,6 +36,47 @@ namespace Explorer.Controllers
             await context.SaveChangesAsync();
             return View();
         }
+        //------------------------//
+        [HttpPost]
+        public async Task<IActionResult> AddFileFromDB([FromForm]IFormFile file)
+        {
+            if(file != null)
+            {
+            }
+            //foreach (IFormFile source in file)
+            //{
+            //    string filename = ContentDispositionHeaderValue.Parse(source.ContentDisposition).FileName.Trim('"');
+
+            //    filename = this.EnsureCorrectFilename(filename);
+
+            //    using (FileStream output = System.IO.File.Create(this.GetPathAndFilename(filename)))
+            //        await source.CopyToAsync(output);
+            //}
+
+            return this.View();
+        }
+
+        private string EnsureCorrectFilename(string filename)
+        {
+            if (filename.Contains("\\"))
+                filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+
+            return filename;
+        }
+
+        private string GetPathAndFilename(string filename)
+        {
+            return hostEnvironment.WebRootPath + "\\uploads\\" + filename;
+        }
+        //------------------------//
+        //[HttpPost]
+        //public async Task<IActionResult> Index(FilesModel file)
+        //{
+        //    //FoldersModel folder = new FoldersModel() { Name = folderName };
+        //    //context.Folders.Add(folder);
+        //    //await context.SaveChangesAsync();
+        //    return View();
+        //}
         //public async Task<IActionResult> Index()
         //{
         //    return View(await context.Folders.ToListAsync());
@@ -47,9 +95,10 @@ namespace Explorer.Controllers
         {
             return View();
         }
+        [HttpPost]
         public IActionResult Add()
         {
-            return View();
+            return Ok("fdsfsd");
         }
     }
 }
---
+++ Explorer/Models/FilesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Explorer.Models
+{
+    public class FilesViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public int? FileExtensionsModelID { get; set; }
+        public int? FoldersModelID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
